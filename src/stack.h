35b94407acb25d7c6df538b5c5b620719f3648eb
archy <archy@yeet.sus>
2023-10-25 19:15:25 +0200

    Corretto un bug che allocava meno memoria di quella necessaria

--- src/stack.h
+++ src/stack.h
@@ -12,14 +12,20 @@
   };                                                                                                                                                           \
   void STACK##_push(STACK *, T);                                                                                                                               \
   void STACK##_pop(STACK *);                                                                                                                                   \
+  STACK *malloc_##STACK(STACK);                                                                                                                                \
   extern const STACK New##STACK;
 
 #define DEFINE_STACK(T, STACK)                                                                                                                                 \
   const STACK New##STACK = {.data = NULL, .capacity = 0, .size = 0, .push = STACK##_push, .pop = STACK##_pop};                                                 \
+  STACK *malloc_##STACK(STACK tmp) {                                                                                                                         \
+    STACK *ptr = malloc(sizeof(T));                                                                                                                            \
+    memcpy(ptr, &tmp, sizeof(T));                                                                                                                            \
+    return ptr;                                                                                                                                                \
+  }                                                                                                                                                            \
   void STACK##_push(STACK *stack, T data) {                                                                                                                    \
-    if (stack->size == stack->capacity) {                                                                                                                      \
+    if (stack->size >= stack->capacity) {                                                                                                                      \
       stack->capacity = stack->size * 1.5 + 1;                                                                                                                 \
-      T *data = realloc(stack->data, stack->capacity);                                                                                                         \
+      T *data = realloc(stack->data, stack->capacity * sizeof(T));                                                                                                         \
       if (!data) {                                                                                                                                             \
         fprintf(stderr, "%s : %s\n", #STACK "_push", "realloc ha fallito");                                                                                    \
         return;                                                                                                                                                \
@@ -32,7 +38,7 @@
   void STACK##_pop(STACK *stack) {                                                                                                                             \
     if (stack->size <= stack->capacity / 3) {                                                                                                                  \
       stack->capacity /= 2;                                                                                                                                    \
-      T *data = realloc(stack->data, stack->capacity);                                                                                                         \
+      T *data = realloc(stack->data, stack->capacity * sizeof(T));                                                                                                         \
       if (!data) {                                                                                                                                             \
         fprintf(stderr, "%s : %s\n", #STACK "_pop", "realloc ha fallito");                                                                                     \
         return;                                                                                                                                                \
