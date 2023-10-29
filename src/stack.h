39e03b51f46f898a73ddb2db12c46ab936b754c2
lumine <hakupisi>
2023-10-29 11:11:19 +0100

    Rimosso codice inutilizzato

--- src/stack.h
+++ src/stack.h
@@ -12,16 +12,11 @@
   };                                                                                                                   \
   void STACK##_push(STACK *, T);                                                                                       \
   void STACK##_pop(STACK *);                                                                                           \
-  STACK *malloc_##STACK(STACK);                                                                                        \
   extern const STACK New##STACK;
 
 #define DEFINE_STACK(T, STACK)                                                                                         \
   const STACK New##STACK = {.data = NULL, .capacity = 0, .size = 0, .push = STACK##_push, .pop = STACK##_pop};         \
-  STACK *malloc_##STACK(STACK tmp) {                                                                                   \
-    STACK *ptr = malloc(sizeof(T));                                                                                    \
-    memcpy(ptr, &tmp, sizeof(T));                                                                                      \
-    return ptr;                                                                                                        \
-  }                                                                                                                    \
+                                                                                                                       \
   void STACK##_push(STACK *stack, T data) {                                                                            \
     if (stack->size >= stack->capacity) {                                                                              \
       stack->capacity = stack->size * 1.5 + 1;                                                                         \
@@ -32,9 +27,9 @@
       }                                                                                                                \
       stack->data = data;                                                                                              \
     }                                                                                                                  \
-    /*stack->data[stack->size++] = data;*/                                                                             \
     memcpy(stack->data + stack->size++, &data, sizeof(T));                                                             \
   }                                                                                                                    \
+                                                                                                                       \
   void STACK##_pop(STACK *stack) {                                                                                     \
     if (stack->size <= stack->capacity / 3) {                                                                          \
       stack->capacity /= 2;                                                                                            \
@@ -53,6 +48,7 @@
     typeof(STACK_P) stack = STACK_P;                                                                                   \
     stack->push(stack, DATA);                                                                                          \
   })
+
 #define pop(STACK_P)                                                                                                   \
   ({                                                                                                                   \
     typeof(STACK_P) stack = STACK_P;                                                                                   \
