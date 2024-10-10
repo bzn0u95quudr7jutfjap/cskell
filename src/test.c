4c8de3b43f82f72f8b3dca2fa7efc1b37cbb9fb9
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-10-10 20:57:23 +0200

    test_token.c

--- src/test.c
+++ src/test.c
@@ -11,49 +11,9 @@ u0 free_test(testdata *t) { t->vtable.free(t); }
 u8 success(testdata *t) { return t->code == CODE_OK; }
 char *status(testdata *t) { return success(t) ? "OK" : "ERR"; }
 
-define_test(
-    test_tokenizer,
-    {
-      t->tokenizer.function(&t->tokenizer.input);
-      t->code = equals_formatter_strings(&t->tokenizer.input, &t->tokenizer.atteso);
-    },
-    {
-      printf("%6s :: %s\n", "INIZIO", t->name);
-      printf("%6s :: [%3s] :: %10s :: ", "STATUS", status(t), "Atteso");
-      print_stack_string(&t->tokenizer.atteso);
-      printf("\n");
-      printf("%6s :: [%3s] :: %10s :: ", "STATUS", status(t), "Output");
-      print_formatter(&t->tokenizer.input);
-      printf("\n");
-      switch (t->code) {
-      case CODE_NULL_RESULT:
-        printf("%6s :: [%3s] :: %s\n", "STATUS", status(t), "CODE_NULL_RESULT");
-        printf("%6s :: [%3s] :: %10s :: %p\n", "STATUS", status(t), "Atteso", &t->tokenizer.atteso);
-        printf("%6s :: [%3s] :: %10s :: %p\n", "STATUS", status(t), "Output", &t->tokenizer.input);
-        break;
-      case CODE_TOKEN_STR_DIFFER:
-        printf("%6s :: [%3s] :: %s\n", "STATUS", status(t), "CODE_TOKEN_STR_DIFFER");
-        break;
-      case CODE_TOKEN_LEN_DIFFER:
-        printf("%6s :: [%3s] :: %s\n", "STATUS", status(t), "CODE_TOKEN_LEN_DIFFER");
-        printf("%6s :: [%3s] :: %10s :: LEN : %6lu\n", "STATUS", status(t), "Atteso", t->tokenizer.atteso.size);
-        printf("%6s :: [%3s] :: %10s :: LEN : %6lu\n", "STATUS", status(t), "Output", t->tokenizer.input.tokens.size);
-        break;
-      default:
-        break;
-      }
-      printf("%6s :: %s\n", "FINE", t->name);
-    },
-    {
-      free_Formatter(&t->tokenizer.input);
-      free_Stack_String(&t->tokenizer.atteso);
-    });
-
-u0 resize_string(String *s, u32 cap) { s->data = realloc(s->data, s->capacity = cap); }
-
 u0 print_string(String *s) {
   String buffer = new_String();
-  resize_string(&buffer, s->size * 2);
+  resize(&buffer, s->size * 2);
   for (u32 i = 0; i < s->size; i++) {
     char c = *at_String(s, i);
     if (32 < c && c < 127) {
